0cde12f4d59a0df6fd27fd8713c0c8e60fb556a3
pavelhym <pavelhym@gmail.com>
2021-11-18 03:53:01 +0300

    Let me die

--- Lab3/Lab3.py
+++ Lab3/Lab3.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
 import copy
-
+import scipy.optimize
 
 alpha_real,beta_real = np.random.rand(2)
 
@@ -20,6 +20,16 @@ for k in range(0,101):
     x_k.append(k/100)
     y_k.append(alpha_real*k/100 + beta_real)
     #np.random.normal(loc=0,scale= 1, size=1)[0]
+
+
+data = pd.read_csv("data.csv")
+
+x_k = data["x_k"]
+y_k = data['y_k']
+y_k_real = data['y_k_real']
+
+plt.plot(x_k,y_k_real)
+plt.scatter(x_k,y_k)
 
 
 
@@ -64,34 +74,31 @@ def rational_grad(params):
     func_grad_var2 = [-2*(1+var2*x)**(-2)*x *var1* (var1/(1+var2*x)-y) for x,y in zip(x_k, y_k)]
     return sum(func_grad_var1), sum(func_grad_var2)
 
+def rational_grad(params):
+    var1,var2 = params[0], params[1]
+    func_grad_var1 =  [2/(1 + var2*x) * (var1/(1+var2*x)-y) for x,y in zip(x_k, y_k)]
+    func_grad_var2 = [-2*(1+var2*x)**(-2)*x *var1* (var1/(1+var2*x)-y) for x,y in zip(x_k, y_k)]
+    return sum(func_grad_var1), sum(func_grad_var2)
 
 
 def rational_grad2(params):
     var1,var2 = params[0], params[1]
     func_grad_var1 =  [-2*(-var1 + var2*x*y + y)/(var2*x+1)**2 for x,y in zip(x_k, y_k)]
-    func_grad_var2 = [-((2*var1*x*(var1 - y*(var2*x+1)))/(var2*x+1)**3) for x,y in zip(x_k, y_k)]
+    #func_grad_var2 = [-((2*var1*x*(var1 - y*(var2*x+1)))/(var2*x+1)**3) for x,y in zip(x_k, y_k)]
+    func_grad_var2 = [-(2*var1*x*(var1-var2*x*y-y)/(var2*x+1)**3) for x,y in zip(x_k, y_k)]
+    return sum(func_grad_var1), sum(func_grad_var2)
+
+
+def rational_grad3(params):
+    var1,var2 = params[0], params[1]
+    func_grad_var1 =  [-2*(-var1 + var2*x*y + y)/(var2*x+1)**2 for x,y in zip(x_k, y_k)]
+    #func_grad_var2 = [-((2*var1*x*(var1 - y*(var2*x+1)))/(var2*x+1)**3) for x,y in zip(x_k, y_k)]
+    func_grad_var2 = [-(var1/(1 + var2*x) -y)*2*x*var1/(1 + var2)**2 for x,y in zip(x_k, y_k)]
     return sum(func_grad_var1), sum(func_grad_var2)
 
 
 
-
-#For 3d plot
-a_varianst = np.linspace(0,1,100)
-b_variants = np.linspace(0,1,100)
-[a_grid, b_grid] = np.meshgrid(a_varianst,b_variants)
-f_grid =  linear([a_grid,b_grid])
-
-a_varianst_rational = np.linspace(0,1,100)
-b_variants_rational = np.linspace(-1,0,100)
-[a_grid_rational, b_grid_rational] = np.meshgrid(a_varianst_rational,b_variants_rational)
-f_grid_rational =  rational([a_grid_rational,b_grid_rational])
-
-
-
-
-
-def gradient_descent(fx_grad, eta=0.0001, num_iterations=1000, a_init = 0.5, b_init = 0.5):
-    
+def gradient_descent(fx_grad, eta=0.0001, num_iterations=1000, a_init = 0, b_init = 0):
     
     a_list=[]
     b_list=[]
@@ -100,13 +107,22 @@ def gradient_descent(fx_grad, eta=0.0001, num_iterations=1000, a_init = 0.5, b_i
     a = a_init
     b = b_init
     f_calc = 0
- 
+    a0 = np.array([a,b])
+    a_n = np.array([1,1])
+    beta = 0.001
+
     for i in range(num_iterations):
+        if i ==0:
+            beta = np.array([0.001,0.001])
+        else:
+            beta = np.abs((a_n - a0)*(np.array(fx_grad(a_n)) - np.array(fx_grad(a0))))/np.linalg.norm((np.array(fx_grad(a_n)) - np.array(fx_grad(a0))))**2
+        print(beta)
         grad_a, grad_b =  fx_grad([a,b])[0], fx_grad([a,b])[1]
         f_calc += 2
-        
+        a0 = np.array([a,b])
         a = a - eta * grad_a
         b = b - eta * grad_b
+        a_n = np.array([a,b]) 
         #print(a)
         a_list.append(a)
         b_list.append(b)
@@ -114,47 +130,21 @@ def gradient_descent(fx_grad, eta=0.0001, num_iterations=1000, a_init = 0.5, b_i
     return a_list[-1], b_list[-1], num_iterations, f_calc
 
 
-a_min1_grad, b_min1_grad, iter1_grad, f_calc1_grad  = gradient_descent(linear_grad,eta=0.001, num_iterations=1000)
-f_x_min1_gauss = linear([a_min1_grad, b_min1_grad])
-
-fig = plt.figure()
-graph = fig.add_subplot(111, projection='3d')
-risunok = graph.plot_surface(a_grid,b_grid,f_grid,cmap=cm.coolwarm,alpha=0.7)
-graph.scatter(a_min1_grad, b_min1_grad, f_x_min1_gauss, c='red',marker='o',s=50)
-graph.set_xlabel('a Label')
-graph.set_ylabel('b Label')
-graph.set_zlabel('Error Label')
-graph.view_init(30, 135)
-fig.colorbar(risunok, shrink=0.2, aspect=10)
-plt.title("Grad descent linear")
-plt.show()
-
-
-#rational
-
-a_min2_grad, b_min2_grad, iter2_grad, f_calc2_grad  = gradient_descent(rational_grad,eta=0.001, num_iterations=1000)
-f_x_min2_gauss = linear([a_min2_grad, b_min2_grad])
 
 
 
-fig = plt.figure(figsize= [12.8, 9.6])
-graph = fig.add_subplot(111, projection='3d')
-risunok = graph.plot_surface(a_grid_rational,b_grid_rational,f_grid_rational,cmap=cm.coolwarm,alpha=0.7)
-graph.scatter(a_min2_grad, b_min2_grad, f_x_min2_gauss, c='red',marker='o',s=50)
-graph.set_xlabel('a Label')
-graph.set_ylabel('b Label')
-graph.set_zlabel('Error Label')
-graph.view_init(30, 135)
-fig.colorbar(risunok, shrink=0.2, aspect=10)
-plt.title("Gradient decent rational")
-plt.show()
 
+
+
+
+
+# Conjugate Gradient
 
 def dichotomy(fx,a,b,eps):
     a_0 = a
     b_0 = b
-    x_1 = (a_0 + b_0 - eps/20)/2
-    x_2 = (a_0 + b_0 + eps/20)/2
+    x_1 = (a_0 + b_0 - eps/2)/2
+    x_2 = (a_0 + b_0 + eps/2)/2
     f_calc_dic1 = 0
     iter_dic1 = 1
     a_1 = a
@@ -169,19 +159,14 @@ def dichotomy(fx,a,b,eps):
 
         f_calc_dic1 += 2
         iter_dic1 +=1
-        x_1 = (a_1 + b_1 - eps/20)/2
-        x_2 = (a_1 + b_1 + eps/20)/2
+        x_1 = (a_1 + b_1 - eps/2)/2
+        x_2 = (a_1 + b_1 + eps/2)/2
         
     x_min_dic1 = (b_1 + a_1)/2
     f_x_min_dic1 = fx(x_min_dic1)
     return x_min_dic1, f_x_min_dic1, iter_dic1, f_calc_dic1
 
 
-
-
-
-
-# Conjugate Gradient
 
 def minima_conj_linear(a):
     params = a_0 + a*s_n
@@ -192,13 +177,12 @@ def minima_conj_rational(a):
     params = a_0 + a*s_n
     return rational(params)
 
-fx_grad = rational_grad2
+fx_grad = rational_grad
 fx_min = minima_conj_rational
 
 
-def conj_gradient(fx_grad,fx_min,a_init=0.5,b_init=0.5,n=100):
+def conj_gradient(fx_grad,fx_min,a_init=1.1,b_init=-0.2,n=100):
 
-    
     global a_0
     a_0 = [a_init,b_init]
     
@@ -207,7 +191,8 @@ def conj_gradient(fx_grad,fx_min,a_init=0.5,b_init=0.5,n=100):
     #for func
     global s_n
     s_n = delta_a_0
-    dich = dichotomy(fx_min,-1000,1000,0.001)
+    dich = dichotomy(fx_min,-10000,10000,0.001)
+    
     alph_0 =  dich[0]
 
     a_0 = a_0 + alph_0*delta_a_0
@@ -231,25 +216,36 @@ def conj_gradient(fx_grad,fx_min,a_init=0.5,b_init=0.5,n=100):
         
         #print(beta)
         
+        print(s_0)
         s_n = np.array(delta_a_n + beta*s_0)
-        dich = dichotomy(fx_min,-1000,1000,0.001)
+        #print(s_n)
+        dich = dichotomy(fx_min,-10000,10000,0.001)
+        #print(dichotomy(fx_min,-1,1,0.001)[1]) 
         alph_0 =  dich[0]
 
         a_0 = np.array(a_0 + alph_0*s_n)
-        print(rational(a_0))
-        s_0 = s_n
-        delta_a_0 = delta_a_n
+        #print(a_0)
+        s_0 = copy.deepcopy(s_n)
+        delta_a_0 = copy.deepcopy(delta_a_n)
         f_calc += dich[3]
+        print(rational(a_0))
     return a_0[0],a_0[1],  iter, f_calc
 
 
 
 #linear
-conj_gradient(linear_grad, minima_conj_linear, n=20)
+conj_gradient(linear_grad, minima_conj_linear, n=100)
 
 #rational
-conj_gradient(rational_grad2, minima_conj_rational,a_init=0.5,b_init=-0.5, n=15)
-#DO NOT WORK
+conj_gradient(rational_grad2, minima_conj_rational, n=8)
+
+#packages
+
+scipy.optimize.minimize(linear, [0,0], method = 'CG',    options={'maxiter': 1000})
+scipy.optimize.minimize(rational , [0,0], method = 'CG', options={'maxiter': 1000})
+
+
+
 
 
 
@@ -274,8 +270,8 @@ def Hessian_rational(params):
     H[1][1] = sum([x**2*((6*var1**2)/(var2*x+1)**4 - (4*var1*y)/(var2*x+1)**3) for x,y in zip(x_k, y_k)])
     return H
 
-a_init = 0.5
-b_init = 0.5
+a_init = 0
+b_init = 0
 a_0 = [a_init,b_init]
 
 #linear
@@ -287,7 +283,6 @@ for i in range(0,100):
 
 
 
-
 #Rational
 #НЕ РАБОТАЕТ 
 for i in range(0,10):
@@ -295,6 +290,13 @@ for i in range(0,10):
    a_1 = a_0 - 0.1*np.linalg.inv(Hessian_rational(a_0)).dot(np.array(rational_grad(a_0)))
    print(rational(a_1))
    a_0 = a_1
+
+
+#packages
+
+scipy.optimize.minimize(linear, [0,0],jac =linear_grad, hess = Hessian_linear , method = 'Newton-CG',          options={'maxiter': 1000})
+scipy.optimize.minimize(rational , [0,0],jac = rational_grad, hess =Hessian_rational , method = 'Newton-CG', options={'maxiter': 1000})
+
 
 
 
@@ -341,3 +343,109 @@ for i in range(0,15):
     print(linear([beta_init[0][0],beta_init[0][1]]))
 
 #НЕ РАБОТАЕТ 
+
+#packages
+
+
+def linear_LMA(params):
+    var1,var2 = params[0], params[1]
+    #print(y_k)
+    #print(x_list)
+    #print(var1)
+    #print(var2)
+    f_pred = [var1*x+var2 for x in x_k]
+    error =  [(var1 - var2)**2 for var1, var2 in zip(f_pred, y_k)]
+    return error
+
+def rational_LMA(params):
+    var1,var2 = params[0], params[1]
+    f_pred = [var1/(1+x*var2) for x in x_k]
+    error =  [(var1 - var2)**2 for var1, var2 in zip(f_pred, y_k)]
+    return error
+
+
+
+scipy.optimize.least_squares(linear_LMA, [1,-0.3],loss='linear' )
+scipy.optimize.least_squares(rational_LMA, [1,-0.3],loss='linear')
+
+
+
+#RESULTS
+
+#LINEAR
+
+
+#GRADIENT DESCENT
+
+
+a_grad_lin, b_grad_lin, iter_grad_lin, f_calc_grad_lin  = gradient_descent(linear_grad,eta=0.001, num_iterations=100)
+f_min_grad_lin = linear([a_grad_lin, b_grad_lin])
+
+print("Gradient descent linear a and b are: ",a_grad_lin , b_grad_lin)
+print("Gradient descent linear f_min is -  ",f_min_grad_lin)
+print("Gradient descent linear iter and f_calc are:  ",iter_grad_lin,f_calc_grad_lin )
+grad_lin_line = [a_grad_lin*x+b_grad_lin for x in x_k]
+
+
+#3d graph
+
+
+a_varianst_scaled = np.linspace(0.6,0.63,100)
+b_variants_scaled = np.linspace(0.89,0.91,100)
+[a_grid_scaled, b_grid_scaled] = np.meshgrid(a_varianst_scaled,b_variants_scaled)
+f_grid_scaled =  linear([a_grid_scaled,b_grid_scaled])
+
+fig = plt.figure(figsize= [12.8, 9.6])
+graph = fig.add_subplot(111, projection='3d')
+risunok = graph.plot_surface( a_grid_scaled,b_grid_scaled,f_grid_scaled, cmap=cm.coolwarm,alpha=0.7)
+graph.scatter(a_grad_lin, b_grad_lin, f_min_grad_lin, c='red',marker='o',s=50, label = "Bruteforce",alpha=1)
+#graph.scatter(a_gauss_lin, b_gauss_lin, f_min_gauss_lin, c='blue',marker='o',s=50, label = "Gauss")
+#graph.scatter(a_nelder_lin, b_nelder_lin, f_min_nelder_lin, c='green',marker='o',s=10, label = "Nelder-Mead")
+graph.set_xlabel('a Label')
+graph.set_ylabel('b Label')
+graph.set_zlabel('Error Label')
+graph.view_init(30, 135)
+graph.legend()
+fig.colorbar(risunok, shrink=0.2, aspect=10)
+plt.title("Linear results comparison scaled")
+#plt.savefig('Plots/3d_linear_small.png')
+plt.show()
+
+
+
+#rational
+
+a_grad_rat, b_grad_rat, iter_grad_rat, f_calc_grad_rat  = gradient_descent(rational_grad,eta=0.001, num_iterations=30)
+f_min_grad_rat = rational([a_grad_rat, b_grad_rat])
+
+print("Gradient descent a and b are: ",a_grad_rat , b_grad_rat)
+print("Gradient descent f_min is -  ",f_min_grad_rat)
+print("Gradient descent iter and f_calc are:  ",iter_grad_rat,f_calc_grad_rat)
+grad_rat_line = [a_grad_rat/(1+x*b_grad_rat) for x in x_k]
+
+
+
+
+
+
+#3d graph
+a_varianst = np.linspace(0.9,1.1,100)
+b_variants = np.linspace(-0.35,-0.25,100)
+[a_grid, b_grid] = np.meshgrid(a_varianst,b_variants)
+f_grid =  rational([a_grid,b_grid])
+
+fig = plt.figure(figsize= [12.8, 9.6])
+graph = fig.add_subplot(111, projection='3d')
+risunok = graph.plot_surface( a_grid,b_grid,f_grid, cmap=cm.coolwarm,alpha=0.7)
+graph.scatter(a_grad_rat, b_grad_rat, f_min_grad_rat, c='red',marker='o',s=50, label = "Bruteforce",alpha=1)
+#graph.scatter(a_gauss_rat, b_gauss_rat, f_min_gauss_rat, c='blue',marker='o',s=10, label = "Gauss")
+#graph.scatter(a_nelder_rat, b_nelder_rat, f_min_nelder_rat, c='green',marker='o',s=10, label = "Nelder-Mead")
+graph.set_xlabel('a Label')
+graph.set_ylabel('b Label')
+graph.set_zlabel('Error Label')
+graph.view_init(30, 65)
+graph.legend()
+fig.colorbar(risunok, shrink=0.3, aspect=10)
+plt.title("Rational results comparison")
+#plt.savefig('Plots/3d_rat.png')
+plt.show()
